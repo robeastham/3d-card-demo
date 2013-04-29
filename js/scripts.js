dc3e6ba0401cc33273eeaa8595aa09c7703e6132
Drew Peifer <peiftronica@gmail.com>
2013-04-29 15:55:43 -0400

    moved QAD useragent detection

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,31 @@ var uA = navigator.userAgent,
     port = $(window);
 // end global variables
 
+// start QAD browser detection
+if (uA.match(/Firefox\/.*/)) {
+    $('body').addClass('firefox');
+} else if (uA.match(/Mozilla\/.*Fennec.*/)) {
+    $('body').addClass('firefox mobile');
+} else if (uA.match(/Chrome\/.*/)) {
+    $('body').addClass('chrome');
+} else if (uA.match(/Chrome\/.*Mobile.*/)) {
+    $('body').addClass('chrome mobile');
+} else if (uA.match(/Safari\/.*/)) {
+    $('body').addClass('safari');
+} else if (uA.match(/.*iPhone\/.*/)) {
+    $('body').addClass('safari mobile iphone');
+} else if (uA.match(/iPad\/.*/)) {
+    $('body').addClass('safari mobile ipad');
+} else if (uA.match(/MSIE 9\.0.*/)) {
+    $('body').addClass('ie9');
+} else if (uA.match(/MSIE 8\.0.*/)) {
+    $('body').addClass('ie8');
+} else if (uA.match(/MSIE 7\.0.*/)) {
+    $('body').addClass('ie7');
+}
+console.log('navigator.useragent = ' + uA);
+// end browser detection
+
 // global functions
 function foldCard() {
     cardBack.css({ transformOrigin : '0px 0px' })
