c02b905f3c271d5cba640ca19ce64ff68e0221d4
Drew Peifer <peiftronica@gmail.com>
2013-05-06 19:30:08 -0400

    added mobile warning [wip]

--- js/browser.js
+++ js/browser.js
@@ -1,26 +1,44 @@
-var uA = navigator.userAgent;
+var uA = navigator.userAgent,
+    body = $('body');
 
 // start QAD browser detection
     if (uA.match(/Firefox\/.*/)) {
-        $('body').addClass('firefox');
+        body.addClass('firefox');
     } else if (uA.match(/Mozilla\/.*Fennec.*/)) {
-        $('body').addClass('firefox mobile');
+        body.addClass('firefox mobile');
     } else if (uA.match(/Chrome\/.*/)) {
-        $('body').addClass('chrome');
+        body.addClass('chrome');
     } else if (uA.match(/Chrome\/.*Mobile.*/)) {
-        $('body').addClass('chrome mobile');
-    } else if (uA.match(/Safari\/.*/)) {
-        $('body').addClass('safari');
-    } else if (uA.match(/.*iPhone\/.*/)) {
-        $('body').addClass('safari mobile iphone');
+        body.addClass('chrome mobile');
+    } else if (uA.match(/.*iPhone.*/)) {
+        body.addClass('safari mobile iphone');
     } else if (uA.match(/iPad\/.*/)) {
-        $('body').addClass('safari mobile ipad');
+        body.addClass('safari mobile ipad');
+    } else if (uA.match(/Safari\/.*/)) {
+        body.addClass('safari');
     } else if (uA.match(/MSIE 9\.0.*/)) {
-        $('body').addClass('ie9');
+        body.addClass('ie9');
     } else if (uA.match(/MSIE 8\.0.*/)) {
-        $('body').addClass('ie8');
+        body.addClass('ie8');
     } else if (uA.match(/MSIE 7\.0.*/)) {
-        $('body').addClass('ie7');
+        body.addClass('ie7');
     }
     console.log('navigator.useragent = ' + uA);
 // end browser detection
+
+// mobile layout rendering
+
+if (body.hasClass('mobile')) {
+    var mobileWarning = '<p class="mobile">Turn your device sideways for the best experience</p>';
+
+    $('.body').prepend(mobileWarning),
+    $('p.mobile').fadeOut();
+};
+
+function hideWarning() {
+    $('p.mobile').slideUp(1500);
+}
+
+$(document).ready(function() {
+    $('p.mobile').fadeIn(2000,hideWarning);
+});
